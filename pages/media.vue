dcdd80b4c73a2184d3e5ef118cc3bb38af4e4ba3
Joseph Keable <keablejoseph@gmail.com>
2026-01-11 17:37:38 +0000

    adds more content

--- pages/media.vue
+++ pages/media.vue
@@ -1,15 +1,19 @@
 <template>
   <VContainer>
-    <div class="d-flex">
-      <VCard v-for="item in media" :key="item.id" class="mx-3" width="300px" outlined>
+    <div class="d-flex align-content-start flex-wrap">
+      <VCard v-for="item in media" :key="item.id" class="mx-3 mt-3" width="300px" outlined>
         <VImg
           color="surface-variant"
           height="200"
-          src="https://cdn.vuetifyjs.com/docs/images/cards/purple-flowers.jpg"
+          width="300"
+          :src="item.thumbnail"
           cover
         />
-
         <VCardTitle>{{ item.title }}</VCardTitle>
+        <VCardText>
+          <p>File Type: {{ item.file_type }}</p>
+          <p>Size: {{ item.size }}</p>
+        </VCardText>
       </VCard>
     </div>
   </VContainer>
@@ -17,10 +21,14 @@
 
 <script setup>
 const media = [
-  { id: 1, title: "Media 1" },
-  { id: 2, title: "Media 2" },
-  { id: 3, title: "Media 3" },
-  { id: 4, title: "Media 4" },
+  { id: 1, title: "Media1.mp4", file_type: "video/mp4", size: "15MB", thumbnail: "https://picsum.photos/id/1/200/300" },
+  { id: 2, title: "Media2.mp4", file_type: "video/mp4", size: "20MB", thumbnail: "https://picsum.photos/id/2/200/300" },
+  { id: 3, title: "Media3.mp4", file_type: "video/mp4", size: "25MB", thumbnail: "https://picsum.photos/id/3/200/300" },
+  { id: 4, title: "Media4.mp4", file_type: "video/mp4", size: "30MB", thumbnail: "https://picsum.photos/id/4/200/300" },
+  { id: 5, title: "Media1.mp4", file_type: "video/mp4", size: "15MB", thumbnail: "https://picsum.photos/id/5/200/300" },
+  { id: 6, title: "Media2.mp4", file_type: "video/mp4", size: "20MB", thumbnail: "https://picsum.photos/id/6/200/300" },
+  { id: 7, title: "Media3.mp4", file_type: "video/mp4", size: "25MB", thumbnail: "https://picsum.photos/id/7/200/300" },
+  { id: 8, title: "Media4.mp4", file_type: "video/mp4", size: "30MB", thumbnail: "https://picsum.photos/id/8/200/300" },
 ];
 </script>
 <style scoped></style>
